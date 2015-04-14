d1f83752b14f65e038af9dd33a9cf925ad989c44
Jaikumar Bhambhwani <jai.bhambhwani@gmail.com>
2015-04-14 14:14:18 -0700

    Update pods to swift 1.3

---
+++ TimeTraveler/WebServices/AbstractNetworkService/AbstractWebServiceClient.swift
@@ -0,0 +1,9 @@
+//
+//  AbstractWebServiceClient.swift
+//  TimeTraveler
+//
+//  Created by Jaikumar Bhambhwani on 4/14/15.
+//  Copyright (c) 2015 Jaikumar Bhambhwani. All rights reserved.
+//
+
+import Foundation
